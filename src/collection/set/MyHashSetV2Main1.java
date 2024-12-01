96f425f4b6d0f7ae6ee95ecce11df410252c5e60
sseregit <sseregit@naver.com>
2024-12-02 05:01:35 +0900

    직접 구현하는 Set2 - MyHashSetV2

---
+++ src/collection/set/MyHashSetV2Main1.java
@@ -0,0 +1,24 @@
+package collection.set;
+
+public class MyHashSetV2Main1 {
+
+    public static void main(String[] args) {
+        MyHashSetV2 set = new MyHashSetV2(10);
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        set.add("D");
+        set.add("AB");
+        set.add("SET");
+        System.out.println(set);
+
+        System.out.println("\"A\".hashCode() = " + "A".hashCode());
+        System.out.println("\"B\".hashCode() = " + "B".hashCode());
+        System.out.println("\"C\".hashCode() = " + "C".hashCode());
+        System.out.println("\"D\".hashCode() = " + "D".hashCode());
+        System.out.println("\"AB\".hashCode() = " + "AB".hashCode());
+        System.out.println("\"SET\".hashCode() = " + "SET".hashCode());
+
+        System.out.println("set.contains(\"SET\") = " + set.contains("SET"));
+    }
+}
