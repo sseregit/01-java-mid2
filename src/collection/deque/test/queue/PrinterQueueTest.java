5f1dee383e49440d4392d86298ec0085e6f9b9ae
장연식 <sseregit@knowwheresoft.com>
2024-12-02 18:03:03 +0900

    문제와 풀이4 - Queue

---
+++ src/collection/deque/test/queue/PrinterQueueTest.java
@@ -0,0 +1,20 @@
+package collection.deque.test.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class PrinterQueueTest {
+
+    public static void main(String[] args) {
+        Queue<String> printQueue = new ArrayDeque<>();
+
+        // 코드 작성
+        printQueue.offer("doc1");
+        printQueue.offer("doc2");
+        printQueue.offer("doc3");
+
+        System.out.println("출력: " + printQueue.poll());
+        System.out.println("출력: " + printQueue.poll());
+        System.out.println("출력: " + printQueue.poll());
+    }
+}
---
+++ src/collection/deque/test/queue/TaskScheduler.java
@@ -0,0 +1,22 @@
+package collection.deque.test.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class TaskScheduler {
+
+    private Queue<Task> tasks = new ArrayDeque<>();
+
+    public void addTask(Task task) {
+        tasks.offer(task);
+    }
+
+    public int getRemainingTasks() {
+        return tasks.size();
+    }
+
+    public void processNextTask() {
+        Task task = tasks.poll();
+        task.execute();
+    }
+}
