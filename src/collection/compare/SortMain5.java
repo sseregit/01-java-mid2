1f22fe0a9416042955cf2e1996bef02d235e513b
sseregit <sseregit@naver.com>
2024-12-03 05:54:45 +0900

    정렬3 - Comparable, Comparator

---
+++ src/collection/compare/SortMain5.java
@@ -0,0 +1,25 @@
+package collection.compare;
+
+import java.util.TreeSet;
+
+public class SortMain5 {
+
+    public static void main(String[] args) {
+        MyUser myUser1 = new MyUser("a", 30);
+        MyUser myUser2 = new MyUser("b", 20);
+        MyUser myUser3 = new MyUser("c", 10);
+
+        TreeSet<MyUser> treeSet = new TreeSet<>();
+        treeSet.add(myUser1);
+        treeSet.add(myUser2);
+        treeSet.add(myUser3);
+        System.out.println(treeSet);
+
+        TreeSet<MyUser> treeSet2 = new TreeSet<>(new IdComparator());
+        treeSet2.add(myUser1);
+        treeSet2.add(myUser2);
+        treeSet2.add(myUser3);
+        System.out.println(treeSet2);
+
+    }
+}
